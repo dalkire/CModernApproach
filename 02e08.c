72ac7f32c0a961029528c617d4b75a9d1c51030e
David Alkire <dalkire@npr.org>
2015-07-13 09:13:07 -0400

    02e08

---
+++ 02e08.c
@@ -0,0 +1,27 @@
+/*
+ * Ch02 Ex08 | C Programming, A Modern Approach
+ *
+ * How many tokens are there in the following statement?
+ * answer=(3*q-p*p)/3;
+ */
+int main(void)
+{
+	return 0;
+}
+
+/*
+ *  1) answer
+ *  2) =
+ *  3) (
+ *  4) 3
+ *  5) *
+ *  6) q
+ *  7) -
+ *  8) p
+ *  9) *
+ * 10) p
+ * 11) )
+ * 12) /
+ * 13) 3
+ * 14) ;
+ */
