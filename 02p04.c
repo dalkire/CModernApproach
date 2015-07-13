a50e90bf2ade209790f2d5c211b94a19903bd7fe
David Alkire <dalkire@npr.org>
2015-07-13 10:00:25 -0400

    02p04

---
+++ 02p04.c
@@ -0,0 +1,20 @@
+/*
+ * Ch02 Pr04 | C Programming, A Modern Approach
+ *
+ * Write a program that asks the user to enter a dollars-and-cents
+ * amount, then displays the amount with 5% tax added:
+ * Enter an amount: 100.00
+ * With tax added: $105.00
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	float amount;
+
+	printf("Enter an amount: ");
+	scanf("%f", &amount);
+	printf("With tax added: $%.2f\n", amount + amount * .05);
+
+	return 0;
+}
