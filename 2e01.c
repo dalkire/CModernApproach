8177d8b58c359145c9eece4e143fac40250bd84f
David Alkire <dalkire@npr.org>
2015-07-12 20:01:02 -0400

    Initial commit.

---
+++ 2e01.c
@@ -0,0 +1,20 @@
+/*
+ * Ch2 Ex1
+ *
+ * Create and run Kernighan and Ritchie's famous
+ * "hello, world" program.
+ *
+ * Do you get a warning message from the compiler?
+ * If so, what's needed to make it go away?
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world\n");
+}
+
+/*
+ * No warning messages using:
+ * gcc -Wall -g 2e01.c -o bin/2e01
+ */
