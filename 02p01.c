53b378916541e888450378d748bfef3e88181185
David Alkire <dalkire@npr.org>
2015-07-13 09:36:58 -0400

    02p01

---
+++ 02p01.c
@@ -0,0 +1,25 @@
+/*
+ * Ch02 Pr01 | C Programming, A Modern Approach
+ *
+ * Write a program that uses printf to display the following
+ * picture on the screen:
+ *        *
+ *       *
+ *      *
+ * *   *
+ *  * *
+ *   *
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	printf("       *\n");
+	printf("      *\n");
+	printf("     *\n");
+	printf("*   *\n");
+	printf(" * *\n");
+	printf("  *\n");
+
+	return 0;
+}
