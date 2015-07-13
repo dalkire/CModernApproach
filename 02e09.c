0dda52406069d060e37c1ff4b83d7e9336b3159a
David Alkire <dalkire@npr.org>
2015-07-13 09:15:39 -0400

    02e09

---
+++ 02e09.c
@@ -0,0 +1,15 @@
+/*
+ * Ch02 Ex09 | C Programming, A Modern Approach
+ *
+ * Insert spaces between the tokens in Exercise 8 to make the
+ * statement easier to read.
+ * answer=(3*q-p*p)/3;
+ */
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * answer = ( 3 * q - p * p ) / 3 ;
+ */
