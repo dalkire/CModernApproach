260627b4201c6ef6444524f42b4b3b47e65b9bb8
David Alkire <dalkire@npr.org>
2015-07-12 20:22:43 -0400

    2e02

---
+++ 2e02.c
@@ -0,0 +1,25 @@
+/*
+ * Consider the following program:
+ *
+ * (a) Identify the directives and statements in this program.
+ * (b) What output does the program produce?
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Parkinson's Law:\nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+	return 0;
+}
+
+/*
+ * (a) Directives: 7
+ *     Statements: 11, 12, 13, 14
+ *     
+ * (b) Parkinson's Law:
+ *     Work expands so as to fill the time
+ *     available for its completion.
+ *     
+ */
