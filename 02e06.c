8bb7edc5d4710cb66a71e7aa333ce6a61aa6c88d
David Alkire <dalkire@npr.org>
2015-07-13 08:31:09 -0400

    02e06

---
+++ 02e06.c
@@ -0,0 +1,18 @@
+/*
+ * Ch02 Ex06 | C Programming, A Modern Approach
+ *
+ * Why is it not a good idea for an identifier to contain
+ * more than one adjacent underscore (as in current__balance,
+ * for example)?
+ */
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * It is too easy to misread two underscores as one and
+ * even though they look very similar at a glance,
+ * current_balance and current__balance are two different
+ * variables.
+ */
