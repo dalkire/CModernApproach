7dff8f33eebf67e78174e4b8678cb07f8492ba4b
David Alkire <dalkire@npr.org>
2015-07-14 19:28:58 -0400

    04e05

---
+++ ch04/04e05.c
@@ -0,0 +1,27 @@
+/*
+ * Ch04 Ex05 | C Programming, A Modern Approach
+ *
+ * What is the value of each of the following expressions
+ * in C89? (Give all possible values if an expression may
+ * have more than one value.)
+ * (a) 8 % 5
+ * (b) -8 % 5
+ * (c) 8 % -5
+ * (d) -8 % -5
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * For modulo, the C89 standard states that for i % j, if
+ * either i or j is negative, the sign of the result depends
+ * on implementation.
+ * (a) 3
+ * (b) -3, 2
+ * (c) -3, 2
+ * (d) 3
+ */
