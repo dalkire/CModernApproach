88ed7491d1e7c417b2f33b1c60223757f49e9b51
David Alkire <dalkire@npr.org>
2015-07-14 19:17:57 -0400

    04e03

---
+++ ch04/04e03.c
@@ -0,0 +1,27 @@
+/*
+ * Ch04 Ex03 | C Programming, A Modern Approach
+ *
+ * What is the value of each of the following expressions
+ * in C89? (Give all possible values if an expression may
+ * have more than one value.)
+ * (a) 8 / 5
+ * (b) -8 / 5
+ * (c) 8 / -5
+ * (d) -8 / -5
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * For division, the C89 standard states that if either operand
+ * is negative, the result of a division can be rounded either
+ * up or down.
+ * (a) 1
+ * (b) -1, -2
+ * (c) -1, -2
+ * (d) 1, 2
+ */
