fd06b7a2289e878691a7de759ea2daa0c7343558
David Alkire <dalkire@npr.org>
2015-07-14 19:19:43 -0400

    04e04

---
+++ ch04/04e04.c
@@ -0,0 +1,26 @@
+/*
+ * Ch04 Ex04 | C Programming, A Modern Approach
+ *
+ * What is the value of each of the following expressions
+ * in C99? (Give all possible values if an expression may
+ * have more than one value.)
+ * (a) 8 / 5
+ * (b) -8 / 5
+ * (c) 8 / -5
+ * (d) -8 / -5
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * For division, the C99 standard states that the result
+ * of division is always truncated toward zero.
+ * (a) 1
+ * (b) -1
+ * (c) -1
+ * (d) 1
+ */
