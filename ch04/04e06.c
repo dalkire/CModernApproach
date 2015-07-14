53e3914918f0ef3a9e6d3fdb1e44a25daf3e5dd7
David Alkire <dalkire@npr.org>
2015-07-14 19:30:42 -0400

    04e06

---
+++ ch04/04e06.c
@@ -0,0 +1,26 @@
+/*
+ * Ch04 Ex06 | C Programming, A Modern Approach
+ *
+ * What is the value of each of the following expressions
+ * in C99? (Give all possible values if an expression may
+ * have more than one value.)
+ * (a) 8 % 5
+ * (b) -8 % 5
+ * (c) 8 % -5
+ * (d) -8 % -5
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * The C99 standard states that for i % j, the result always
+ * has the same sign as i.
+ * (a) 3
+ * (b) -3
+ * (c) 2
+ * (d) -3
+ */
