4d9bc77b7192f042fccc0df2c51d2d176510e163
David Alkire <dalkire@npr.org>
2015-07-13 20:45:13 -0400

    03e03

---
+++ ch03/03e03.c
@@ -0,0 +1,24 @@
+/*
+ * Ch03 Ex03 | C Programming, A Modern Approach
+ *
+ * For each of the following pairs of scanf format strings,
+ * indicate whether or not the two strings are equivalent.
+ * If they're not, show how they can be distinguished.
+ * (a) "%d"       versus " %d"
+ * (b) "%d-%d-%d" versus "%d -%d -%d"
+ * (c) "%f"       versus "%f "
+ * (d) "%f,%f"    versus "%f, %f"
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * (a) Equivalent
+ * (b) Equivalent
+ * (c) Equivalent
+ * (d) Equivalent
+ */ 
