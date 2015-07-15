44cbcad5bbaf4228bac047405406c0439ebb81ed
David Alkire <dalkire@npr.org>
2015-07-14 20:51:29 -0400

    04e08

---
+++ ch04/04e08.c
@@ -0,0 +1,16 @@
+/*
+ * Ch04 Ex08 | C Programming, A Modern Approach
+ *
+ * Would the upc.c program still work if the expression
+ * 9 - ((total - 1) % 10) were replaced by (10 - (total % 10)) % 10?
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * The expressions are equivalent for all totals > 0.
+ */
