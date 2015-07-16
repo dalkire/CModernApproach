c16376c742935b1d7500851979cf2de4bb6a22be
David Alkire <dalkire@npr.org>
2015-07-16 16:25:25 -0400

    05e09

---
+++ ch05/05e09.c
@@ -0,0 +1,37 @@
+/*
+ * Ch05 Ex09 | C Programming, A Modern Approach
+ *
+ * Are the following if statements equivalent? If not, why not?
+ * (a) if (score >= 90)
+ *         printf("A");
+ *     else if (score >= 80)
+ *         printf("B");
+ *     else if (score >= 70)
+ *         printf("C");
+ *     else if (score >= 60)
+ *         printf("D");
+ *     else
+ *         printf("F");
+ *
+ * (b) if (score < 60)
+ *         printf("F");
+ *     else if (score < 70)
+ *         printf("D");
+ *     else if (score < 80)
+ *         printf("C");
+ *     else if (score < 90)
+ *         printf("B");
+ *     else
+ *         printf("A");
+
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * The statements are equivalent.
+ */ 
