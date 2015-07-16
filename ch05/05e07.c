39b5b6fb562a3ffeabf41043c62ea423447f2c53
David Alkire <dalkire@npr.org>
2015-07-16 16:10:08 -0400

    05e07

---
+++ ch05/05e07.c
@@ -0,0 +1,34 @@
+/*
+ * Ch05 Ex07 | C Programming, A Modern Approach
+ *
+ * What does the following statement print if i has the value 17?
+ * What does it print if i has the value -17?
+ * printf("%d\n", i >= 0 ? i : -i);
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	i = 17;
+	printf("%d\n", i >= 0 ? i : -i);
+
+	i = -17;
+	printf("%d\n", i >= 0 ? i : -i);
+
+	return 0;
+}
+
+/*
+ * (a) i = 17;
+ *     i >= 0 ? i : -i
+ *     17 >= 0 ? 17 : -17
+ *     1 ? 17 : -17
+ *     17
+ * (b) i = -17
+ *     i >= 0 ? i : -i
+ *     -17 >= 0 ? -17 : -(-17)
+ *     0 ? -17 : -(-17)
+ *     17
+ */
