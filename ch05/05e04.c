e7ed20d1dda294c7de89c33d7933548bf3a9848f
David Alkire <dalkire@npr.org>
2015-07-16 13:43:22 -0400

    Cleanup

--- ch05/05e04.c
+++ ch05/05e04.c
@@ -1,5 +1,5 @@
 /*
- * Ch05 Pr04 | C Programming, A Modern Approach
+ * Ch05 Ex04 | C Programming, A Modern Approach
  *
  * Write a single expression whose value is either -1, 0, or +1,
  * depending on whether i is less than, equal to, or greater than j,
