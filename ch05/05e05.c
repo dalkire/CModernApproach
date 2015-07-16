afa2f745ba463c71cc4840e87c4005427fcf0e0d
David Alkire <dalkire@npr.org>
2015-07-16 15:15:27 -0400

    05e05

---
+++ ch05/05e05.c
@@ -0,0 +1,28 @@
+/*
+ * Ch05 Ex05 | C Programming, A Modern Approach
+ *
+ * Is the following if statement legal?
+ * if (n >= 1 <= 10)
+ *     printf("n is between 1 and 10\n");
+ *
+ * If so, what does it do when n is equal to 0?     
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+
+	if (n >= 1 <= 10)
+		printf("n is between 1 and 10\n");
+
+	return 0;
+}
+
+/*
+ * It is legal.
+ * Relational operators are left associative, so n >= 1 <= 10 is:
+ * (n >= 1) <= 10
+ * (n >= 1) is either 0 or 1. Both are <= 10, so the expression
+ * always returns 1, in other words the if statement will always be true.
+ */
