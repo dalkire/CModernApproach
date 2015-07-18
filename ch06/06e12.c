4456eeb793253adbcf49c44a511c083d25c0d302
David Alkire <dalkire@npr.org>
2015-07-18 18:41:26 -0400

    06e12

---
+++ ch06/06e12.c
@@ -0,0 +1,22 @@
+/*
+ * Ch06 Ex12 | C Programming, A Modern Approach
+ *
+ * The following "prime-testing" loop appeared in Section 6.4 as an example:
+ * for (d = 2; d < n; d++)
+ *     if (n % d == 0)
+ *         break;
+ *
+ * This loop isn't very efficient. It's not necessary do divide n by all
+ * numbers between 2 and n - 1 to determine whether it's prime. In fact, we
+ * need only check divisors up to the square root of n. Modify the loop to
+ * take advantage of this fact. Hint: Don't try to compute the square root
+ * of n; instead compare d * d with n.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+
+	return 0;
+}
