82cbf2fe384a567e4d7ad21218a31a104e54570c
David Alkire <dalkire@npr.org>
2015-07-18 20:58:30 -0400

    06p04

---
+++ ch06/06p04.c
@@ -0,0 +1,54 @@
+/*
+ * Ch06 Pr04 | C Programming, A Modern Approach
+ *
+ * Add a loop to the broker.c program of Section 5.2 so that the user can
+ * enter more than one trade and the program will calculate the commission
+ * on each. The program should terminate when the user enters 0 as the
+ * trade value:
+ * Enter value of trade: 30000
+ * Commission: $166.00
+ *
+ * Enter value of trade: 20000
+ * Commission $144.00
+ *
+ * Enter value of trade: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float commission, value;
+
+	printf("Terminate program by entering 0\n");
+
+	while (1) {
+		printf("\nEnter value of trade: ");
+		scanf("%f", &value);
+
+		if (value == 0) {
+			break;
+		}
+
+		if (value < 2500.00f) {
+			commission = 30.00f + .017f * value;
+		} else if (value < 6250.00f) {
+			commission = 56.00f + .0066f * value;
+		} else if (value < 20000.00f) {
+			commission = 76.00f + .0034f * value;
+		} else if (value < 50000.00f) {
+			commission = 100.00f + .0022f * value;
+		} else if (value < 500000.00f) {
+			commission = 155.00f + .0011f * value;
+		} else {
+			commission = 255.00f + .0009f * value;
+		}
+
+		if (commission < 39.00f) {
+			commission = 39.00f;
+		}
+
+		printf("Commission: $%.2f\n", commission);
+	}
+
+	return 0;
+}
