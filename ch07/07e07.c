4accad7ae1e4b9667d9aae1e0f553ccf188dd39e
David Alkire <dalkire@npr.org>
2015-07-20 16:38:04 -0400

    07e07

---
+++ ch07/07e07.c
@@ -0,0 +1,24 @@
+/*
+ * Ch07 Ex07 | C Programming, A Modern Approach
+ *
+ * For each of the following character escapes, give the equivalent octal
+ * escape. (Assume the character set is ASCII.) You may wish to consult
+ * Appendix E, which lists the numerical codes for ASCII characters.
+ * (a) \b
+ * (b) \n
+ * (c) \r
+ * (b) \t
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * (a) b = 98 => \0142
+ * (b) n = 110 => \0156
+ * (c) r = 114 => \0162
+ * (d) t = 116 => \0164
+ */
