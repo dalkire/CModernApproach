8a512fbc18a0f8fa4b3931180db58a7471a8bda7
David Alkire <dalkire@npr.org>
2015-07-20 17:42:00 -0400

    07e11

---
+++ ch07/07e11.c
@@ -0,0 +1,17 @@
+/*
+ * Ch07 Ex11 | C Programming, A Modern Approach
+ *
+ * Suppose that i is a variable of type int, f is a variable of type float,
+ * and d is a variable of type double. What is the type of the expression
+ * i * f / d?
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * i * f is float, and float / double is double.
+ */
