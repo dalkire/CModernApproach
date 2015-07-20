eed45f19955ffdd05779cec39f9421cddd9cfad9
David Alkire <dalkire@npr.org>
2015-07-20 15:42:47 -0400

    07e03

---
+++ ch07/07e03.c
@@ -0,0 +1,19 @@
+/*
+ * Ch07 Ex03 | C Programming, A Modern Approach
+ *
+ * Which of the following are not legal types in C?
+ * (a) short unsigned int
+ * (b) short float
+ * (c) long double
+ * (d) unsigned long
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * (b) short float is not a legal type in C.
+ */
