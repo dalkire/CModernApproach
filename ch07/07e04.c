adf16b7a3b2a9788f5daa9d0b9f58da6f5d92956
David Alkire <dalkire@npr.org>
2015-07-20 15:57:48 -0400

    07e04

---
+++ ch07/07e04.c
@@ -0,0 +1,20 @@
+/*
+ * Ch07 Ex04 | C Programming, A Modern Approach
+ *
+ * If c is a variable of type char, which one of the following statements
+ * is illegal?
+ * (a) i += c; // i has type int
+ * (b) c = 2 * c - 1;
+ * (c) putchar(c);
+ * (d) printf(c);
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * (d) printf(c) is illegal because no formatted string is given.
+ */
