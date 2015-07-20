c815b8020bfac1e706580fe385c89c3dc35a5da5
David Alkire <dalkire@npr.org>
2015-07-20 17:44:15 -0400

    07e12

---
+++ ch07/07e12.c
@@ -0,0 +1,18 @@
+/*
+ * Ch07 Ex12 | C Programming, A Modern Approach
+ *
+ * Suppose that i is a variable of type int, f is a variable of type float,
+ * and d is a variable of type double. Explain what conversions take place
+ * during the execution of the following statement:
+ * d = i + f;
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+/*
+ * i + f is float, and that float is converted to a double for the assignment.
+ */
