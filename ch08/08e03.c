8f781024e1109fe29c85cacae6b6426c777cf852
David Alkire <dalkire@npr.org>
2015-07-23 07:02:21 -0400

    08e03

---
+++ ch08/08e03.c
@@ -0,0 +1,22 @@
+/*
+ * Ch08 Ex03 | C Programming, A Modern Approach
+ *
+ * Write a declaration of an array named weekend containing seven bool values.
+ * Include an initializer that makes the first and last values true; all
+ * other values should be false;
+ */
+#include <stdio.h>
+#include <stdbool.h>
+
+#define DAYS_IN_WEEK 7
+
+int main(void)
+{
+	bool weekend[DAYS_IN_WEEK] = {[0] = true, [6] = true};
+
+	for (int i = 0; i < DAYS_IN_WEEK; i++) {
+		printf("%d is a weekend day: %s\n", i, weekend[i] ? "true" : "false");
+	}
+	
+	return 0;
+}
